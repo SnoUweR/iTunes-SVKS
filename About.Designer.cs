5a5e71ecb5bdef1e805cabb2f9904096552104d1
SnoUweR <snouwer@gmail.com>
2013-09-28 20:56:47 +0800

    1.1 was changed to 1.2 in properties

--- About.Designer.cs
+++ About.Designer.cs
@@ -44,7 +44,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "iTunes SVKS 1.1";
+            this.label1.Text = "iTunes SVKS 1.2";
             // 
             // linkLabel1
             // 
